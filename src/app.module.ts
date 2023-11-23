33cdd916c772fd4156384e776e5a51eb829710a3
yusuke0729 <siroha323@gmail.com>
2023-11-23 15:19:20 +0900

    add: 性格診断のエンドポイント追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { DiagnosisModule } from './diagnosis/diagnosis.module';
 
 @Module({
-	imports: [PrismaModule, AuthModule],
+	imports: [PrismaModule, AuthModule, DiagnosisModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
